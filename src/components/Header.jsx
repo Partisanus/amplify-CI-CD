651cca25ee19691a7283f0a0946b67cc0643e2d3
raman.hrynkevich <raman.hrynkevich@azati.com>
2022-03-01 12:49:17 +0300

    added form1

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { PageHeader, Divider } from 'antd'
+
+const Header = () => {
+	return(
+		<>
+		<PageHeader title="AntDesign Demo" />
+		<Divider />
+		</>
+	)
+}
+
+
+
+export default Header
