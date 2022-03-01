c9018fcea397fe79791237f5170f416d8a238c0a
raman.hrynkevich <raman.hrynkevich@azati.com>
2022-03-01 14:48:56 +0300

    added form6

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
           <div key={note.id || note.name}>
             <h2>{note.name}</h2>
             <p>{note.description}</p>
-            <button onClick={() => deleteTodo(note)}>Delete note!</button>
+            <button onClick={() => deleteTodo(note)}>Delete note</button>
             {
               note.image && <img src={note.image} style={{width: 400}} />
             }
